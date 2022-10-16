15425d4b87a991f5e92a01a42dbbed88f4da9a01
Felix Hoffmann <felix.emmanuel.hoffmann@gmail.com>
2022-10-16 11:04:33 +0200

    🌱 Added a file for every api

---
+++ src/utils/http_request.py
@@ -0,0 +1,26 @@
+from typing import Optional
+import requests
+from loguru import logger
+from typing import Optional
+
+
+def http_request(url: str, timeout: int = 10) -> Optional[str]:
+    """Send a HTTP request to the given URL and return the response.
+
+    Args:
+        url (str): The URL of the API.
+        timeout (int, optional): The time in seconds to wait for a response. Defaults to 10.
+
+    Returns:
+        Optional[str]: The response from the API or None if the request failed.
+    """
+    try:
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+    except requests.HTTPError as http_err:
+        logger.error(f"HTTP error occurred: {http_err}")
+    except Exception as err:
+        logger.error(f"Other error occurred: {err}")
+    else:
+        print("Success!")
+        return response.json()

e6c71c2538d45f1b51c18157eea5c53a7b5fd4b9
steadl <steadl@gmx.de>
2022-11-24 20:07:04 +0100

    ♻️ Moved calendar functions from notebook to python file

---
+++ aswe/api/calendar/calendar.py
@@ -0,0 +1,158 @@
+import os.path
+import pickle
+from datetime import datetime, timedelta
+from typing import Any
+
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from loguru import logger
+
+from aswe.api.calendar.data import Event
+
+SCOPES = ["https://www.googleapis.com/auth/calendar"]
+CREDENTIALS_FILE = "../../../../credentials.json"
+PICKLE_FILE = "../../../../token.pickle"
+
+
+def get_calendar_service() -> Any:
+    """Provides a Resource object to interact with the Google Calendar API
+
+    Returns
+    -------
+    Any
+        Resource for interacting with Google Calendar
+    """
+    creds = None
+    if os.path.exists(PICKLE_FILE):  # stores access tokens once created (automatically)
+        with open(PICKLE_FILE, "rb") as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        # Save the credentials for the next run
+        with open(PICKLE_FILE, "wb") as token:
+            pickle.dump(creds, token)
+
+    service = build("calendar", "v3", credentials=creds)
+    return service
+
+
+def get_events_by_timeframe(min_timestamp: str, max_timestamp: str) -> list[Event]:
+    """Provides all events inside timeframe
+
+    Parameters
+    ----------
+    min_timestamp : str
+        Lower timestamp in timeframe with format "yyyy-MM-ddTHH:mm:ss.ffffffZ"
+    max_timestamp : str
+        Higher timestamp in timeframe with format "yyyy-MM-ddTHH:mm:ss.ffffffZ"
+
+    Returns
+    -------
+    list[Event]
+        List of all events inside timeframe
+    """
+    service = get_calendar_service()
+
+    calendars = service.calendarList().list().execute().get("items", [])  # type: ignore
+    event_array = []
+    for i in enumerate(calendars):
+        current_calendar = calendars[i].get("id", "")
+        if current_calendar != "":
+
+            events_result = (
+                service.events()
+                .list(
+                    calendarId=current_calendar,
+                    timeMin=min_timestamp,
+                    timeMax=max_timestamp,
+                    maxResults=100,
+                    singleEvents=True,
+                    orderBy="startTime",
+                )
+                .execute()
+            )
+
+            events = events_result.get("items", [])
+
+            for event_data in events:
+                event = Event(
+                    title=event_data.get("summary", ""),
+                    description=event_data.get("description", ""),
+                    location=event_data.get("location", ""),
+                    full_day="date" in event_data.get("start", {}),
+                    date=event_data.get("start", {}).get("date", ""),
+                    start_time=event_data.get("start", {}).get("dateTime", ""),
+                    end_time=event_data.get("end", {}).get("dateTime", ""),
+                )
+                event_array.append(event)
+
+    return event_array
+
+
+def get_all_events_today() -> list[Event]:
+    """Provides list of all events happening today
+
+    Returns
+    -------
+    list[Event]
+        List of all events happening today
+    """
+    today_min = datetime.today().strftime("%Y-%m-%dT00:00:00.000001Z")
+    today_max = datetime.today().strftime("%Y-%m-%dT23:59:59.999999Z")
+
+    return get_events_by_timeframe(today_min, today_max)
+
+
+def get_next_event_today() -> Event | None:
+    """Provides the next event happening today
+
+    Returns
+    -------
+    Event|None
+        Next event happening today if one exists
+    """
+    events = get_all_events_today()
+
+    for event in events:
+        if not event.full_day:
+            start_time = datetime.strptime(event.startTime, "%Y-%m-%dT%H:%M:%S+01:00")
+            if datetime.now() < start_time:
+                return event
+    return None
+
+
+def create_event(event_info: Event) -> None:
+    """Creates Event in Google Calendar
+
+    Parameters
+    ----------
+    eventInfo : Event
+        Information about the event that is created
+    """
+    if event_info.full_day:
+        end_date = (datetime.fromisoformat(event_info.date) + timedelta(days=1)).strftime("%Y-%m-%d")
+    else:
+        end_date = ""
+
+    event = {
+        "summary": event_info.title,
+        "location": event_info.location,
+        "description": event_info.description,
+        "start": {"date": event_info.date}
+        if event_info.full_day
+        else {"dateTime": event_info.startTime, "timeZone": "Europe/Berlin"},
+        "end": {"date": end_date}
+        if event_info.full_day
+        else {"dateTime": event_info.endTime, "timeZone": "Europe/Berlin"},
+    }
+
+    service = get_calendar_service()
+    service.events().insert(calendarId="primary", body=event).execute()
+
+    logger.success(f"Created Event: {event}")
---
+++ aswe/api/calendar/data.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Event:
+    """Dataclass storing Event data
+
+    Parameters
+    ----------
+    title : str
+        The title of the event
+    description : str
+        The description of the event
+    location : str
+        The location the event takes place
+    full_day : bool
+        Stores if the event is active the entire day
+    date : str
+        The date of a full-day event with the format "yyyy-MM-dd"
+    startTime : str
+        The start time of a non-full-day event with the format "yyyy-MM-ddTHH:mm:ss+01:00"
+    endTime : str
+        The end time of a non-full-day event with the format "yyyy-MM-ddTHH:mm:ss+01:00"
+    """
+
+    title: str
+    description: str
+    location: str
+    full_day: bool
+    date: str
+    start_time: str
+    end_time: str

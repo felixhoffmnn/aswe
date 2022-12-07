3104ad59dfc828102ed556e9b4b756429697268e
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-12-07 12:50:41 +0100

    🔊 add log level loading

--- aswe/__init__.py
+++ aswe/__init__.py
@@ -1,3 +1,13 @@
+import os
+import sys
+
 from dotenv import load_dotenv
+from loguru import logger
 
 load_dotenv()
+
+LOG_LEVEL = os.getenv("LOGURU_LEVEL", "TRACE")
+
+if LOG_LEVEL in ["DEBUG", "INFO", "SUCCESS", "WARNING", "ERROR", "CRITICAL"]:
+    logger.remove()
+    logger.add(sys.stderr, level=LOG_LEVEL)
